49702fa867f60641913a0b3a89f68edbaddb86a8
Rogerio Alves <sigmamix10@gmail.com>
2023-02-24 17:36:13 -0300

    alterações operadores

---
+++ src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        // x repetição
+
+        int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado = "";
+
+        resultado = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
+    }
+}
