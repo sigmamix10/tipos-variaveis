45c6ebc45286d30218813288ce238f72f18241f0
Rogerio Alves <sigmamix10@gmail.com>
2023-02-25 10:58:46 -0300

    arquivo comentarios e calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+    //Comentario de linha única
+
+    /*
+     * Comentario de linhas
+     * multiplas
+     */
+
+     /**
+      * Comentario de documentação
+      Olha só
+      */
+}
