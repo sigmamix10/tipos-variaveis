955afa785d5f6dc75b0c56bf33e7ebb770c365c3
Rogerio Alves <sigmamix10@gmail.com>
2023-02-25 13:53:56 -0300

    alteracoes de aula terminal

---
+++ src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+   public static void main(String[] args) {
+    int num01 = 0;
+    int num02 = 0;
+
+    if(num01 == num02){
+      System.out.println("Fui compilado pelo Terminal");
+      System.out.println("Ola meu povo");
+    }
+    
+   }
+}
